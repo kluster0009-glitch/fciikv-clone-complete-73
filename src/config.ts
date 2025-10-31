757a6b99b89822f8316e80fae1e3fc94b85aca2e
kluster0009-glitch <kluster0009@gmail.com>
2025-10-31 14:16:06 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "https://kluster-backend-j8s3nmju3-kluster0009-5787s-projects.vercel.app";
+export const BACKEND_URL = "https://kluster-backend.vercel.app/";
 export const AUTH_ENABLED = true;
