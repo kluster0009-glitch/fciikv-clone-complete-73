61bc525f9ee14d8a394964e7b282f05685f1ed6a
kluster0009-glitch <kluster0009@gmail.com>
2025-10-31 14:07:42 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "https://5000-firebase-kluster-backend-1758275720695.cluster-bg6uurscprhn6qxr6xwtrhvkf6.cloudworkstations.dev";
+export const BACKEND_URL = "https://kluster-backend-j8s3nmju3-kluster0009-5787s-projects.vercel.app";
 export const AUTH_ENABLED = true;
